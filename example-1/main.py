3520675e200e755cdf1b6af74e58b353bfda3676
showbiz04 <micka.furude@gmail.com>
2025-10-27 23:34:19 +0300

    Resolve ex 1

--- example-1/main.py
+++ example-1/main.py
@@ -1,12 +1,16 @@
 import sys
 
+
 def main():
     """
     Пример ввода и вывода числа n, где -10^9 < n < 10^9:
     n = int(input())
     print(n)
     """
-    pass
+    n = input()
+    a, b = n.split()
+    c = int(a) + int(b)
+    print(c)
 
 if __name__ == '__main__':
     main()
